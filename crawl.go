279b7f95d148ef04ad00fe7ade99e27d52255adb
Ruslan Moiseev <moiseev_nvr@mail.ru>
2020-10-07 12:55:37 +0300

    add chennel

--- crawl.go
+++ crawl.go
@@ -1,47 +1,56 @@
 package main
 
 import (
-  "crypto/tls"   
-  "flag"         
-  "fmt"
-  "github.com/jackdanger/collectlinks"
-  "net/http"
-  "os"
+	"crypto/tls"
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/jackdanger/collectlinks"
 )
 
 func main() {
-  flag.Parse()
+	flag.Parse()
 
-  args := flag.Args()
-  fmt.Println(args)
-  if len(args) < 1 {
-    fmt.Println("Please specify start page")
-    os.Exit(1)
-  }
+	args := flag.Args()
+	fmt.Println(args)
+	if len(args) < 1 {
+		fmt.Println("Please specify start page")
+		os.Exit(1)
+	}
 
-  tlsConfig := &tls.Config{                 
-                 InsecureSkipVerify: true,  
-               }                            
-                                            
-                                            
+	queue := make(chan string)
 
-  transport := &http.Transport{    
-    TLSClientConfig: tlsConfig,    
-  }                                
+	go func() {
 
-  client := http.Client{Transport: transport}  
-                                               
+		queue <- args[0]
+	}()
 
-  resp, err := client.Get(args[0])  
-  if err != nil {                   
-    return                          
-  }
-  defer resp.Body.Close()
-  
-  links := collectlinks.All(resp.Body)
+	for uri := range queue {
 
-  for _, link := range(links) {
-    fmt.Println(link)
-  }
+		enqueue(uri, queue)
+	}
 }
 
+func enqueue(uri string, queue chan string) {
+	fmt.Println("fetching", uri)
+	tlsConfig := &tls.Config{
+		InsecureSkipVerify: true,
+	}
+	transport := &http.Transport{
+		TLSClientConfig: tlsConfig,
+	}
+	client := http.Client{Transport: transport}
+	resp, err := client.Get(uri)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	links := collectlinks.All(resp.Body)
+
+	for _, link := range links {
+		go func() { queue <- link }()
+	}
+}
